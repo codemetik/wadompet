8a0cd61465eacfd3cdf8583063f23a0f7858655a
codemetik <irfanovaiyalkharish03@gmail.com>
2023-08-01 21:11:16 +0700

    perubahan di index.php

--- index.php
+++ index.php
@@ -2,31 +2,31 @@
 include "koneksi.php";
 
 
-if (isset($_COOKIE['username']) == 0) {
-	header("location:login.php");
-}else if (isset($_COOKIE['username']) > 0) {
-	$user = $_COOKIE['username'];
-	$query = mysqli_query($koneksi, "select * from tb_user where user = '$user'");
-	$data = mysqli_fetch_array($query);
-
-	if ($data['id_level'] == '1') {
-	header("location:admin.php");
-	}else if($data['id_level'] == '2'){
-	header("location:anggota.php");
-	}
-}
-
-// if (!isset($_COOKIE['username'])) {
+// if (isset($_COOKIE['username']) == 0) {
 // 	header("location:login.php");
-// }else if (isset($_COOKIE['username'])){
-//   $user = $_COOKIE['username'];
-//   $query = mysqli_query($koneksi, "select * from tb_user where user = '$user'");
-//   $data = mysqli_fetch_array($query);
+// }else if (isset($_COOKIE['username']) > 0) {
+// 	$user = $_COOKIE['username'];
+// 	$query = mysqli_query($koneksi, "select * from tb_user where user = '$user'");
+// 	$data = mysqli_fetch_array($query);
 
-//   if ($data['id_level'] == '1') {
-//     header("location:admin.php");
-//   }else if($data['id_level'] == '2'){
-//     header("location:anggota.php");
-//   }
+// 	if ($data['id_level'] == '1') {
+// 	header("location:admin.php");
+// 	}else if($data['id_level'] == '2'){
+// 	header("location:anggota.php");
+// 	}
 // }
+
+if (!isset($_COOKIE['username'])) {
+	header("location:login.php");
+}else if (isset($_COOKIE['username'])){
+  $user = $_COOKIE['username'];
+  $query = mysqli_query($koneksi, "select * from tb_user where user = '$user'");
+  $data = mysqli_fetch_array($query);
+
+  if ($data['id_level'] == '1') {
+    header("location:admin.php");
+  }else if($data['id_level'] == '2'){
+    header("location:anggota.php");
+  }
+}
 ?>
